28442c0f43678735bf03dbf5cbc4c2fc954da91a
Misha Wagner <mishajw@gmail.com>
2016-04-30 18:38:39 +0100

    Fixed JSON output

--- src/main/scala/bandhierarchy/back/manipulation/D3GraphExporter.scala
+++ src/main/scala/bandhierarchy/back/manipulation/D3GraphExporter.scala
@@ -1,26 +1,32 @@
 package bandhierarchy.back.manipulation
 
-import bandhierarchy.back.WeightedGigGraph
+import bandhierarchy.back.{Band, Weight, WeightedGigGraph}
 import org.json4s.JsonAST.JValue
 import org.json4s.JsonDSL._
 
 object D3GraphExporter {
   def toJson(g: WeightedGigGraph): JValue = {
-    val gSeq = g.toSeq
-    val gBandSeq = gSeq map { case (b, _) => b }
+    val bands: Seq[Band] = (g.keys ++ g.values.flatMap(_._2)).toSet.toSeq
 
     ("nodes" ->
-      gSeq.map { case (b, (w, s)) =>
+      bands.map { b =>
         ("name" -> b.name) ~
-          ("id" -> b.id) ~
-          ("weight" -> w)
+        ("id" -> b.id) ~
+        ("weight" -> weight(b, g))
       }) ~
     ("links" ->
       g.flatMap { case (b, (w, sup)) =>
         sup.map { s =>
-          ("source" -> (gBandSeq indexOf b)) ~
-          ("destination" -> (gBandSeq indexOf s))
+          ("source" -> (bands indexOf b)) ~
+          ("target" -> (bands indexOf s))
         }
       })
+  }
+
+  private def weight(b: Band, g: WeightedGigGraph): Weight = {
+    g contains b match {
+      case false => 0
+      case true => g(b) match { case (w, _) => w }
+    }
   }
 }
