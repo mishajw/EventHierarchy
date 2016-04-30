6ba7ea4f9da0049c9100a60b666ea0d311accc20
Misha Wagner <mishajw@gmail.com>
2016-04-30 19:15:51 +0100

    D3 working visually

--- src/main/scala/bandhierarchy/front/Graph.scala
+++ src/main/scala/bandhierarchy/front/Graph.scala
@@ -1,31 +1,41 @@
 package bandhierarchy.front
 
 import org.scalajs.dom
-import org.scalajs.dom.{Selection, raw}
-import org.singlespaced.d3js.d3.Primitive
-import org.singlespaced.d3js.forceModule.Force
-import org.singlespaced.d3js.{BaseDom, Link, d3}
-import org.singlespaced.d3js.histogramModule.Bin
+import org.singlespaced.d3js.forceModule.Node
+import org.singlespaced.d3js.{Link, Selection, d3}
 
 import scala.scalajs.js
-import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
+import scala.scalajs.js.{JSApp, UndefOr}
 
 object Graph extends JSApp {
   @js.native
   trait GraphJson extends js.Object {
-    val nodes: js.Array[GraphNode] = js.native
-    val links: js.Array[GraphLink] = js.native
+    val nodes: js.Array[JsonNode] = js.native
+    val links: js.Array[JsonLink] = js.native
   }
 
   @js.native
-  trait GraphNode extends js.Object {
+  trait JsonNode extends js.Object {
     val name: String = js.native
     val id: Int = js.native
     val weight: Double = js.native
   }
 
-  trait GraphLink extends Link[GraphNode]
+  @js.native
+  trait JsonLink extends js.Object {
+    val source: Int = js.native
+    val target: Int = js.native
+  }
+
+  class GraphNode(val name: String, val id: Int, private val _weight: Double) extends Node {
+    weight = _weight
+  }
+
+  class GraphLink(_source: GraphNode, _target: GraphNode) extends Link[GraphNode] {
+    override def source: GraphNode = _source
+    override def target: GraphNode = _target
+  }
 
   val width: Double = 600
   val height: Double = 400
@@ -45,44 +55,56 @@ object Graph extends JSApp {
     d3.json("/target/graph.json", (error: js.Any, jsonRaw: js.Any) => {
       val json = jsonRaw.asInstanceOf[GraphJson]
 
+      val newNodes = json.nodes
+        .map(n => new GraphNode(n.name, n.id, n.weight))
+      val newLinks = json.links
+        .map(l => new GraphLink(newNodes(l.source), newNodes(l.target)))
+
       force
-        .nodes(json.nodes)
-        .links(json.links)
+        .nodes(newNodes)
+        .links(newLinks)
         .start()
 
       val link = svg.selectAll[GraphLink](".link")
-        .data(json.links)
+        .data(newLinks)
         .enter().append("line")
         .attr("class", "link")
-        .style("stroke-width", 1)
 
-      val node = svg.selectAll[GraphNode](".node")
-        .data(json.nodes)
+      link.attr("stroke-width", mkFunction(link, (l: GraphLink, i: Int) => 1))
+
+      val node: Selection[GraphNode] = svg.selectAll[GraphNode](".node")
+        .data(newNodes)
         .enter().append("circle")
         .attr("class", "node")
         .call(force.drag)
 
-//      val radius: node.DatumFunction[d3.Primitive] = (n: GraphNode, i: Int, j: Int) => n.weight.asInstanceOf[d3.Primitive]
-//      node.attr("r", radius)
-//
-//      val title: node.DatumFunction[d3.Primitive] = (n: GraphNode, i: Int, j: Int) => n.name
-//      node.append("title").text(title)
+      node.attr("r", mkFunction(node, (n: GraphNode, i) => n.weight))
+
+      node.append("title").text(mkFunction(node, (n: GraphNode, i) => n.name))
 
       force.on("tick", (e: dom.Event) => {
         link
-          .attr("x1", 0)
-          .attr("y1", 0)
-          .attr("x2", 0)
-          .attr("y2", 0)
+          .attr("x1", mkFunction(link, (n: GraphLink, i) => n.source.x))
+          .attr("y2", mkFunction(link, (n: GraphLink, i) => n.source.y))
+          .attr("x1", mkFunction(link, (n: GraphLink, i) => n.target.x))
+          .attr("y2", mkFunction(link, (n: GraphLink, i) => n.target.y))
 
         node
-          .attr("cx", 0)
-          .attr("cy", 0)
-
+          .attr("cx", mkFunction(node, (n: GraphNode, i) => n.x))
+          .attr("cy", mkFunction(node, (n: GraphNode, i) => n.y))
+        
         ()
       })
 
       ()
     })
+  }
+
+  private def mkFunction[T, R](sel: Selection[T], f: (T, Int) => R) = {
+    val df: sel.DatumFunction[d3.Primitive] = new scala.scalajs.js.Function3[T,Int,scala.scalajs.js.UndefOr[Int],scala.scalajs.js.|[scala.scalajs.js.|[Double,String],Boolean]] {
+      override def apply(t: T, i: Int, j: UndefOr[Int]) = f(t, i).asInstanceOf[d3.Primitive]
+    }
+
+    df
   }
 }
